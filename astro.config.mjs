07b9ed1029c71fabf65ee8dbb7a07c5906eed6f9
lewis <versus7063@gmail.com>
2025-07-15 15:20:25 +0800

    init project

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// https://astro.build/config
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import icon from 'astro-icon'
+
+import { remarkReadingTime } from './src/utils/all'
+
+export default defineConfig({
+	site: 'https://lazy-pod.pages.dev/',
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		rehypePlugins: ['rehype-plugin-image-native-lazy-loading'],
+		extendDefaultPlugins: true,
+	},
+	integrations: [tailwind(), mdx(), sitemap(), icon()],
+	port: '4322'
+})
---
+++ src/data/authors.ts
@@ -0,0 +1,20 @@
+import type { ImageMetadata } from 'astro'
+import aimage from '../assets/authors/elsa.png'
+
+export interface Props {
+	name: string
+	slug: string
+	image: ImageMetadata
+	bio: string
+}
+
+export type Author = Props
+
+export const authors: Props[] = [
+	{
+		name: 'Lewis',
+		slug: 'lewis',
+		image: aimage,
+		bio: '秉持斯多葛哲學，追求理性與自由。',
+	},
+]
