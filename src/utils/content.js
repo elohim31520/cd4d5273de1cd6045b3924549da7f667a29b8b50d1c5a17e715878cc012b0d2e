ffbc065bc6abf4c68bec79065b0e1abc77ceb214
lewis <versus7063@gmail.com>
2025-08-21 12:37:52 +0800

    feat: 新增心靈與投資類別的文章分類頁面

--- src/utils/content.js
+++ src/utils/content.js
@@ -11,3 +11,29 @@ export const latestPosts = (
 		new Date(b.data.publishDate).valueOf() -
 		new Date(a.data.publishDate).valueOf()
 )
+
+export const getSpiritualPosts = (
+	await getCollection('blog', ({ data }) => {
+		const requiredCategories = ['philosophy', 'mental-health', 'psychology']
+		return requiredCategories.some((category) =>
+			data.categories.includes(category)
+		)
+	})
+).sort(
+	(a, b) =>
+		new Date(b.data.publishDate).valueOf() -
+		new Date(a.data.publishDate).valueOf()
+)
+
+export const getInvestmentPosts = (
+	await getCollection('blog', ({ data }) => {
+		const requiredCategories = ['investment']
+		return requiredCategories.some((category) =>
+			data.categories.includes(category)
+		)
+	})
+).sort(
+	(a, b) =>
+		new Date(b.data.publishDate).valueOf() -
+		new Date(a.data.publishDate).valueOf()
+)
