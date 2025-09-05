f8dc0168523ce0028f0842854907920790a89135
lewis <versus7063@gmail.com>
2025-09-05 16:19:11 +0800

    feat: 使用gulp ，打包前固定檢查圖片大小與壓縮

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+import { exec } from 'child_process'
+import { series, task } from 'gulp'
+
+// 顯示子進程的輸出
+function runCommand(command) {
+	return (done) => {
+		const process = exec(command)
+		process.stdout.on('data', (data) => {
+			console.log(data.toString())
+		})
+		process.stderr.on('data', (data) => {
+			console.error(data.toString())
+		})
+		process.on('exit', (code) => {
+			if (code === 0) {
+				done()
+			} else {
+				done(new Error(`Command failed with exit code ${code}`))
+			}
+		})
+	}
+}
+
+// 任務：轉換圖片
+task('convertImages', runCommand('node script/convert-png-to-webp.js'))
+
+// 任務：執行 astro build
+task('astroBuild', runCommand('astro build'))
+
+// 預設任務：先轉換圖片，然後建置
+task('default', series('convertImages', 'astroBuild'))
