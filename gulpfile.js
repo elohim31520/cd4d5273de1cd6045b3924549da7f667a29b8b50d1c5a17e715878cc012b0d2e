a9b97a6af6a79e388bdb91deb461592dc57da258
lewis <versus7063@gmail.com>
2025-09-24 10:42:41 +0800

    feat: gulp流程打包後新增壓縮content與assets檔案 用以備份

--- gulpfile.js
+++ gulpfile.js
@@ -27,5 +27,15 @@ task('convertImages', runCommand('node script/convert-png-to-webp.js'))
 // 任務：執行 astro build
 task('astroBuild', runCommand('astro build'))
 
-// 預設任務：先轉換圖片，然後建置
-task('default', series('convertImages', 'astroBuild'))
+// 新增任務：壓縮 src/content 和 src/assets 資料夾
+task(
+	'compressSrcFolders',
+	runCommand(
+		'rm -f src_backup.7z && 7z a src_backup.7z ./src/content ./src/assets'
+	)
+)
+
+// 預設任務：先轉換圖片，然後建置，最後壓縮指定的 src 資料夾
+task('default', series('convertImages', 'astroBuild', 'compressSrcFolders'))
+
+// dist整坨上傳
